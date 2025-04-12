c21c5aeca2a13d0e2fb590c43937a77249fe336e
Khorshidb06 <khorshid.b06@gmail.com>
2025-04-12 23:24:05 +0330

    Add files via upload

---
+++ controller/MapController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Result;
+
+public class MapController {
+    public Result printMap(String command) {
+
+    }
+
+    public Result help(String command) {
+
+    }
+
+    public Result showEnergy(String command) {
+
+    }
+
+    public Result setEnergy(String command) {
+
+    }
+
+    public Result unlimitedEnergyControl(String command) {
+
+    }
+
+    public Result walkingControl(String command) {
+
+    }
+
+
+}
