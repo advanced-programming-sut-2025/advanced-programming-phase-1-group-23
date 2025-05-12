7e36304b987116cf99d79d2ee1b1f9f650e13fb3
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:30:48 +0330

    for save

--- src/main/java/model/Objects/ShopTool.java
+++ src/main/java/model/Objects/ShopTool.java
@@ -1,12 +1,18 @@
 package src.main.java.model.Objects;
 import model.enums.ToolType;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class ShopTool extends ShopItem {
     private final ToolType type;
 
     public ShopTool(ToolType type, int dailyLimit) {
         super(dailyLimit);
         this.type = type;
+    }
+
+    public ShopTool() {
     }
 
     public ToolType getType() {
