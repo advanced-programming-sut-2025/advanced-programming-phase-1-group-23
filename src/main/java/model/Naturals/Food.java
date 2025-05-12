ff8240d48598a6bfef0b2b628f7fa10ca66ffe43
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:12:00 +0330

    for save

--- src/main/java/model/Naturals/Food.java
+++ src/main/java/model/Naturals/Food.java
@@ -2,9 +2,13 @@ package model.Naturals;
 
 import model.enums.Ingredients;
 import model.enums.Recipe;
+import dev.morphia.annotations.Embedded;
 
+@Embedded
 public class Food {
     private Recipe recipe;
     private Ingredients type;
 
+    public Food() {
+    }
 }
