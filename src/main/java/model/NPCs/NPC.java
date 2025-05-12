8eb7cfb466eafc47cbc1e4bf4ff82bd3eb27cda1
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:09:00 +0330

    for save

--- src/main/java/model/NPCs/NPC.java
+++ src/main/java/model/NPCs/NPC.java
@@ -1,11 +1,13 @@
 package model.NPC;
 
 import java.util.List;
+import dev.morphia.annotations.Embedded;
 
+@Embedded
 public class NPC {
-        private String name;
-        private String job;
-        private List<String> favorites;
+   //     private String name;
+  //      private String job;
+ //       private List<String> favorites;
 
 }
 
