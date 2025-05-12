d3a2b6af657024aafd3c827e20fb4c06fc255b0c
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:29:27 +0330

    for save

--- src/main/java/model/Objects/ShopRecipe.java
+++ src/main/java/model/Objects/ShopRecipe.java
@@ -1,12 +1,18 @@
 package src.main.java.model.Objects;
 import model.enums.Recipe;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class ShopRecipe extends ShopItem {
     private final Recipe type;
 
     public ShopRecipe(Recipe type, int dailyLimit) {
         super(dailyLimit);
         this.type = type;
+    }
+
+    public ShopRecipe() {
     }
 
     public Recipe getType() {
