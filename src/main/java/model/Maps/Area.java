e5d49b90f0bc2b109793ea7b2d84ae90c8a27492
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 08:55:05 +0330

    for save

--- src/main/java/model/Maps/Area.java
+++ src/main/java/model/Maps/Area.java
@@ -1,5 +1,8 @@
 package model.Maps;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class Area {
     private final Position UpperLeftCorner;
     private final Position BottomRightCorner;
