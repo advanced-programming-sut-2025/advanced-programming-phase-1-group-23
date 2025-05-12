5bee6ed6cccf8d51a16e8718eed1297f29d1847c
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 08:57:47 +0330

    for save

--- src/main/java/model/Maps/FodderCrop.java
+++ src/main/java/model/Maps/FodderCrop.java
@@ -1,5 +1,8 @@
 package model.Maps;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class FodderCrop extends Objects{
     public FodderCrop() {
         super(true,"Foddercrop","yellow");
