28fcbc9717635231a6805ecbbd7f5c528145f651
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:34:23 +0330

    for save

--- src/main/java/model/Command.java
+++ src/main/java/model/Command.java
@@ -2,6 +2,9 @@ package model;
 
 import java.util.HashMap;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class Command {
     public final String command;
     public final HashMap<String, String> body = new HashMap<>();
