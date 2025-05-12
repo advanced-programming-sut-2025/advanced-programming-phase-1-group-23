e5e53c5cc073b85293650439e703f3c25e66678d
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:26:47 +0330

    for save

--- src/main/java/model/Objects/ShopAnimal.java
+++ src/main/java/model/Objects/ShopAnimal.java
@@ -1,12 +1,18 @@
 package src.main.java.model.Objects;
 import model.enums.AnimalType;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class ShopAnimal extends ShopItem {
     private final AnimalType type;
 
     public ShopAnimal(AnimalType type, int dailyLimit) {
         super(dailyLimit);
         this.type = type;
+    }
+
+    public ShopAniaml() {
     }
 
     public AnimalType getType() {
