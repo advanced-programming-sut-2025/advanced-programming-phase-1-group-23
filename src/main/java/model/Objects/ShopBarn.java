5b2b19bcf144e23f51e0d50402a01b6acd7d7e26
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-12 09:27:45 +0330

    for save

--- src/main/java/model/Objects/ShopBarn.java
+++ src/main/java/model/Objects/ShopBarn.java
@@ -2,12 +2,18 @@ package src.main.java.model.Objects;
 
 import src.main.java.model.enums.BarnType;
 
+import dev.morphia.annotations.Embedded;
+
+@Embedded
 public class ShopBarn extends ShopItem {
     private final BarnType type;
 
     public ShopBarn(BarnType type, int dailyLimit) {
         super(dailyLimit);
         this.type = type;
+    }
+
+    public ShopBarn() {
     }
 
     public BarnType getType() {
