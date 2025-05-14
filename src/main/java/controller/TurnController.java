29c7ad799268fcf181f8b8ace36302dcd1296794
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-14 22:12:38 +0330

    Update TurnController.java

--- src/main/java/controller/TurnController.java
+++ src/main/java/controller/TurnController.java
@@ -10,68 +10,131 @@ import model.enums.Menus;
 import view.AppView;
 import view.PlayGame;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TurnController extends ControllersController {
     public static boolean isWaitingForChoosingMap = false;
 
+//    public static Resualt handleNewGame(Command request) {
+//        String[] usernames = parseUsernames(request);
+//
+//        ArrayList<Player> players = createPlayersList(usernames);
+//        if (players == null) {
+//            return new Resualt(false, "Oops! Player’s missing or already gaming!");
+//        }
+//
+//        Game game = createGameAndAssign(players);
+//        isWaitingForChoosingMap = true;
+//
+//        return new Resualt(true, "^_^ Game’s all set! Now it’s map-picking time, adventurers!\n" +
+//                "Type 'game map <map_number>' to claim your battleground (1 or 2)!"
+//
+//        );
+//    }
+//
+//    private static String[] parseUsernames(Command request) {
+//        return request.body.get("users").split("\\s+");
+//    }
+//
+//    private static ArrayList<Player> createPlayersList(String[] usernames) {
+//        ArrayList<Player> players = new ArrayList<>();
+//        User loggedInUser = App.getLoggedInUser();
+//        players.add(new Player(loggedInUser));
+//
+//        for (int i = 1; i < usernames.length; i++) {
+//            String username = usernames[i];
+//            User user = UserRepo.findUserByUsername(username);
+//            if (user == null || user.equals(loggedInUser) || user.getCurrentGame() != null) {
+//                return null;
+//            }
+//            players.add(new Player(user));
+//        }
+//        return players;
+//    }
+//
+//    private static Game createGameAndAssign(ArrayList<Player> players) {
+//        Game game = new Game(players, players.get(0));
+//        for (Player player : players) {
+//            player.getUser().setCurrentGame(game);
+//        }
+//        return game;
+//    }
+
     public static Resualt handleNewGame(Command request) {
         String[] usernames = request.body.get("users").split("\\s+");
-        if (usernames.length == 0) {
-            return new Resualt(false, "No players specified");
-        }
 
-        List<Player> players = createPlayerList(usernames);
-        if (players == null) {
-            return new Resualt(false, "Failed to create player list");
-        }
-
-        Game game = initializeGame(players);
-        setupPlayerGameReferences(players, game);
-
-        isWaitingForChoosingMap = true;
-        return new Resualt(true,
-                "Game successfully created! Now we wait for players to pick their map like kids choosing candy...");
-    }
-
-    private static List<Player> createPlayerList(String[] usernames) {
-        List<Player> players = new ArrayList<>();
-        User currentUser = App.getLoggedInUser();
-        players.add(new Player(currentUser));
-
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(App.getLoggedInUser()));
         for (int i = 1; i < usernames.length; i++) {
             String username = usernames[i];
             User user = UserRepo.findUserByUsername(username);
-
             if (user == null) {
-                return null;
+                return new Resualt(false, "User not found");
             }
-            if (user.equals(currentUser)) {
-                return null;
+            if (user.equals(App.getLoggedInUser())) {
+                return new Resualt(false, "You can't add yourself.");
             }
             if (user.getCurrentGame() != null) {
-                return null;
+                return new Resualt(false, "Player " + username + " is already in a game");
             }
-
-            players.add(new Player(user));
+            Player player = new Player(user);
+            players.add(player);
         }
-        return players;
-    }
-
-    private static Game initializeGame(List<Player> players) {
-        return new Game((ArrayList<Player>) players, players.get(0));
-    }
-
-    private static void setupPlayerGameReferences(List<Player> players, Game game) {
+        Game game = new Game(players, players.get(0));
         for (Player player : players) {
-            User user = player.getUser();
-            user.setCurrentGame(game);
-            user.getGames().add(game);
-            user.setNumberOfGamesPlayed(user.getNumberOfGamesPlayed() + 1);
+            player.getUser().setCurrentGame(game);
         }
+        isWaitingForChoosingMap = true;
+        return new Resualt(true, "The game has been made successfully. Awaiting each user's map choice...\n" +
+                "Use 'game map <map_number>' to pick map 1 or 2.");
     }
 
+    //    public static Resualt handleMapSelection(Command request) {
+//        User user = App.getLoggedInUser();
+//        Game game = user.getCurrentGame();
+//        Player player = game.getCurrentPlayer();
+//
+//        int mapNumber;
+//        try {
+//            mapNumber = Integer.parseInt(request.body.get("mapNumber"));
+//        } catch (NumberFormatException e) {
+//            return new Resualt(false, "That's not even a number! Are you trying to break the game?");
+//        }
+//
+//        if (mapNumber < 1 || mapNumber > 2) {
+//            return new Resualt(false, "Invalid map number. We're not running a farm simulator empire here!");
+//        }
+//
+//        Farm farm = Farm.makeFarm(mapNumber);
+//        game.getMap().addFarm(farm);
+//        player.setFarm(farm);
+//
+//        boolean allPlayersChose = game.cycleToNextPlayer();
+//        if (allPlayersChose) {
+//            isWaitingForChoosingMap = false;
+//        }
+//
+//        String responseString = String.format("%s has chosen Farm #%d. %s",
+//                player.getUser().getUsername(),
+//                mapNumber,
+//                mapNumber == 1 ? "The classic choice!" : "Living life on the edge!");
+//
+//        List<User> users = game.getPlayers().stream()
+//                .map(Player::getUser)
+//                .collect(Collectors.toList());
+//
+//
+//        if (allPlayersChose) {
+//            responseString += "\n\nAll farms selected! Let the agricultural warfare begin!";
+//            GameRepo.saveGame(game, (ArrayList<User>) users);
+//            game.setGameOngoing(true);
+//
+//            users.forEach(UserRepo::saveUser);
+//        }
+//
+//        return new Resualt(true, responseString);
+//    }
     public static Resualt handleMapSelection(Command request) {
         User user = App.getLoggedInUser();
         Game game = user.getCurrentGame();
@@ -88,38 +151,41 @@ public class TurnController extends ControllersController {
             isWaitingForChoosingMap = false;
         }
         String responseString = player.getUser().getUsername() + " has chosen their farm.";
-        ArrayList<User> users = new ArrayList<>();
-        for (Player player1 : game.getPlayers()) {
-            users.add(player1.getUser());
-        }
         if (check) {
-            responseString += "\nAll farm selection successful! Game successfully created!";
-            GameRepo.saveGame(game);
-            for (User u : users) {
-                UserRepo.saveUser(u);
+            ArrayList<User> users = new ArrayList<>();
+            for (Player player1 : game.getPlayers()) {
+                users.add(player1.getUser());
             }
+            responseString += "\nAll farm selection successful! Game successfully created!";
+            GameRepo.saveGame(game, users);
         }
+        user.setCurrentGame(game);
         return new Resualt(true, responseString);
     }
 
     public static Resualt handleLoadGame(Command request) {
         if (App.getLoggedInUser().getCurrentGame() == null) {
-            return new Resualt(false, "No saved game found.");
+            return new Resualt(false, "No saved game found. Maybe it escaped?");
         }
+
         User user = App.getLoggedInUser();
         Game game = user.getCurrentGame();
         ArrayList<Player> players = game.getPlayers();
+
         Player firstPlayer = players.getFirst();
         Player loader = null;
+
         for (Player player : players) {
             User u = UserRepo.findUserById(player.getId().toString());
             player.setUser(u);
+
             if (player.getUser().equals(user)) {
                 game.setCurrentPlayer(player);
                 loader = player;
                 break;
             }
         }
+
         game.setGameOngoing(true);
         int loaderIndex = players.indexOf(loader);
         players.set(0, loader);
@@ -130,57 +196,79 @@ public class TurnController extends ControllersController {
         game.getGameThread().start();
         GameRepo.saveGame(game);
 
-        return new Resualt(true, "The game has been loaded successfully. Welcome "
-                + user.getUsername());
+        return new Resualt(true,
+                "Game successfully loaded from the digital afterlife!\n" +
+                        "Welcome back, " + user.getUsername() + "!\n" +
+                        "May your strategies be better than your saving habits.");
     }
 
     public static Resualt handleExitGame(Command request) {
-        Game game = App.getLoggedInUser().getCurrentGame();
+        User currentUser = App.getLoggedInUser();
+        Game game = currentUser.getCurrentGame();
 
-        if (game.getCurrentPlayer().getUser().equals(App.getLoggedInUser())) {
-
-            game.setGameOngoing(false);
-            game.getGameThread().keepRunning = false;
-            game.hasTurnCycleFinished = false;
-            GameRepo.saveGame(game);
-            App.setCurrentMenu(Menus.GameMenu);
-
-            return new Resualt(true, "Exiting and saving game. Redirecting to game menu...");
-        } else {
-            return new Resualt(false, "Only the logged in user may exit the game.");
+        if (!game.getCurrentPlayer().getUser().equals(currentUser)) {
+            return new Resualt(false,
+                    "Nice try, " + currentUser.getUsername() + "!\n" +
+                            "Only the current player can exit. Your revolution will not be televised.");
         }
+
+        game.setGameOngoing(false);
+        game.getGameThread().keepRunning = false;
+        game.hasTurnCycleFinished = false;
+
+        GameRepo.saveGame(game);
+
+        App.setCurrentMenu(Menus.GameMenu);
+
+        return new Resualt(true,
+                "Game saved successfully!\n" +
+                        "We'll miss you, " + currentUser.getUsername() + "!\n" +
+                        "Your farm animals will be waiting... probably.");
     }
 
     public static Resualt handleForceDeleteGame(Command request) {
-        Game game = App.getLoggedInUser().getCurrentGame();
         User loggedInUser = App.getLoggedInUser();
+        Game game = loggedInUser.getCurrentGame();
 
-        if (!game.getCurrentPlayer().getUser().equals(App.getLoggedInUser())) {
-            return new Resualt(false, "Only the logged in user can force delete the game.");
+        if (!game.getCurrentPlayer().getUser().equals(loggedInUser)) {
+            return new Resualt(false,
+                    "Access denied! Only the current player can initiate Gamepocalypse.");
         }
 
-        System.out.println("Attempting to delete the game. Initializing voting sequence...");
+        System.out.println("Initiating game deletion protocol. Launching democracy missile...");
         game.cycleToNextPlayer();
 
-        boolean success = false;
-        while (!success) {
-            System.out.println("Awaiting confirmation (Y/n) from player "
-                    + game.getCurrentPlayer().getUser().getUsername());
-            String answer = AppView.scanner.nextLine();
-            if (answer.compareToIgnoreCase("y") == 0) {
-                System.out.println("Confirmation received.");
-                success = game.cycleToNextPlayer();
-            } else {
+        while (true) {
+            Player currentVoter = game.getCurrentPlayer();
+            System.out.printf("Awaiting confirmation (Y/n) from %s: ",
+                    currentVoter.getUser().getUsername());
+
+            String answer = AppView.scanner.nextLine().trim();
+
+            if (!answer.equalsIgnoreCase("Y")) {
                 game.setCurrentPlayer(game.findPlayerByUser(loggedInUser));
-                return new Resualt(true, "Confirmation not received. Aborting...");
+                return new Resualt(true,
+                        "Mutiny! The deletion has been cancelled. Democracy wins this time.");
+            }
+
+            System.out.println("Vote recorded. Passing to next player...");
+            if (game.cycleToNextPlayer()) {
+                break;
             }
         }
+
+        terminateGame(game, loggedInUser);
+        return new Resualt(true,
+                "The game has been erased from existence.\n" +
+                        "Returning to Game Menu. Please don't cry.");
+    }
+
+    private static void terminateGame(Game game, User loggedInUser) {
         game.getGameThread().keepRunning = false;
         game.setGameOngoing(false);
         loggedInUser.setCurrentGame(null);
         loggedInUser.getGames().remove(game);
         GameRepo.removeGame(game);
-        return new Resualt(true, "The game has been deleted successfully. Going to game menu...");
     }
 
     public static Resualt handleNextTurn(Command request) {
@@ -190,7 +278,7 @@ public class TurnController extends ControllersController {
         int numberOfPlayers = game.getPlayers().size();
         int playerIndex;
 
-        game.getCurrentPlayer().setEnergyUsed(0);
+        game.getCurrentPlayer().setUsedEnergyInTurn(0);
 
         String responseString = "";
         do {
@@ -205,12 +293,12 @@ public class TurnController extends ControllersController {
                 responseString +=
                         ("Player " + game.getCurrentPlayer().getUser().getUsername() + " was fainted and skipped.\n");
             }
-            // Fainted
         } while (game.getCurrentPlayer().isFainted());
 
 
         responseString += ("It is " + game.getCurrentPlayer().getUser().getUsername() + "'s turn now!");
+        
         GameRepo.saveGame(game);
-        return new Resualt(true, responseString);
+        return new Resualt(true, responseString + "\n" );
     }
 }
