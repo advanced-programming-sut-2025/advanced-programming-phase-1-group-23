7c0a7ae56784a77d7dc9739c320138c58f052691
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-14 22:30:26 +0330

    Update Plant.java

--- src/main/java/model/Maps/Plant.java
+++ src/main/java/model/Maps/Plant.java
@@ -1,11 +1,19 @@
 package model.Maps;
 
+import model.enums.TreeName;
+
+import java.time.LocalDateTime;
 import dev.morphia.annotations.Embedded;
 
 @Embedded
-public class Plant extends Objects{
+public class Plant extends Objects {
+    private TreeName name;
+    
+    public Plant () {
 
-    public Plant(){
-        super(false,"plant","green");
+    }
+    public Plant(TreeName name) {
+        super(false, "green", "tree");
+        this.name = name;
     }
 }
