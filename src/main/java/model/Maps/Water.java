df940aba230bd304bbd518c8605a2f6281abc5d3
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-14 22:24:56 +0330

    Update Water.java

--- src/main/java/model/Maps/Water.java
+++ src/main/java/model/Maps/Water.java
@@ -1,11 +1,10 @@
 package model.Maps;
-
 import dev.morphia.annotations.Embedded;
 
 @Embedded
 public class Water extends Objects{
 
     public Water(){
-        super(false,"water","blue");
+        super(false,"blue","water");
     }
 }
