d17bca32b913a993715c3f8a84b7e3326b95bb25
Yasaman Kavianpour <jasmin.kvp@gmail.com>
2025-05-14 22:42:06 +0330

    Update BuildingsForPaint.java

--- src/main/java/model/Maps/BuildingsForPaint.java
+++ src/main/java/model/Maps/BuildingsForPaint.java
@@ -3,15 +3,17 @@ package model.Maps;
 import dev.morphia.annotations.Embedded;
 
 @Embedded
-public class BuildingsForPaint extends Objects{
+public class BuildingsForPaint extends Objects {
     public String buildingType;
 
     public BuildingsForPaint() {
         super();
     }
 
-    public BuildingsForPaint(boolean canWalk,String buildingType) {
-        super(canWalk,"b","red");
+    public BuildingsForPaint(boolean canWalk, String buildingType) {
+
+        super(canWalk, "red", "b");
+
         this.buildingType = buildingType;
     }
 }
